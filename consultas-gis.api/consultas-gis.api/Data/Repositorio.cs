c8c66d509597bd6619f0de42d3112bdb0792fc3b
dim-desarrollo <118459601+dim-desarrollo@users.noreply.github.com>
2025-09-19 14:45:30 -0300

    asdasd

--- consultas-gis.api/consultas-gis.api/Data/Repositorio.cs
+++ consultas-gis.api/consultas-gis.api/Data/Repositorio.cs
@@ -14,7 +14,7 @@ namespace consultas_gis.api.Data
         {
 
                var sql = @"
-                          SELECT DOCUMENTO, p.APELLIDO_NOMBRE, b.CONCEPTO, CLAVE_BIEN, ANO_CUOTA, NRO_CUOTA
+                          SELECT top 200 DOCUMENTO, p.APELLIDO_NOMBRE, b.CONCEPTO, CLAVE_BIEN, ANO_CUOTA, NRO_CUOTA
 ,case when pb.TIPO_BIEN='CICI' then
 sum(case when TIPO_ITEM='CICITEM2' then isnull(CAPITAL_ITEM,0) end)
 -sum(case when TIPO_ITEM='CICIRETE' then isnull(CAPITAL_ITEM,0)*-1 end) 
